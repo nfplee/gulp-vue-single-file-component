346a753200aa3c7514e90ad8a8eb04b81ab2d962
Lee Timmins <lee.timmins@outlook.com>
2018-06-01 15:35:20 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,154 @@
 # gulp-vue-single-file-component
+
+`gulpfile.js`:
+
+```javascript
+var gulp    		= require('gulp');
+var babel   		= require('gulp-babel');
+var rename  		= require('gulp-rename');
+var vueComponent	= require('gulp-vue-single-file-component');
+
+gulp.task('scripts', function() {
+    return gulp.src('./js/*.js')
+        .pipe(babel({ plugins: 'transform-es2015-modules-amd' })) // Optional line which converts the component to an AMD module
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('vue', function() {
+    return gulp.src('./js/components/*.vue')
+        .pipe(vueComponent({ debug: true, loadCssMethod: 'loadCss' }))
+        .pipe(babel({ plugins: 'transform-es2015-modules-amd' })) // Optional line which converts the component to an AMD module
+        .pipe(rename({extname: '.js'}))
+        .pipe(gulp.dest('./public/js/components'));
+});
+
+gulp.task('default', ['scripts', 'vue']);
+```
+
+`/js/app.js`:
+
+```javascript
+import Vue from 'vue';
+import Hello from './components/Hello';
+import Hello2 from './components/Hello2';
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    },
+    components: {
+        Hello2
+    }
+});
+```
+
+`/js/components/hello.js`:
+
+```html
+<template>
+	<div class="hello">{{ greeting }} <span><slot></slot></span>!</div>
+</template>
+
+<script>
+	import Vue from 'vue';
+
+	export default Vue.component('hello', {
+		data() {
+			return {
+				greeting: 'Hello'
+			}
+		}
+	});
+</script>
+
+<style lang="less">
+	.hello {
+		color: red;
+		
+		span {
+			color: blue;
+		}
+	}
+</style>
+```
+
+`/js/components/hello2.js`:
+
+```html
+<template>
+	<div class="hello2">{{ greeting }} <span>{{name}}</span>!</div>
+</template>
+
+<script>
+	export default {
+		props: ['name'],
+		data() {
+			return {
+				greeting: 'Hello2'
+			}
+		}
+	};
+</script>
+
+<style>
+	.hello2 {
+		color: red;
+    }
+		
+    .hello2 span {
+        color: green;
+    }
+</style>
+```
+
+`/public/index.html`:
+
+```html
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+	<title>Example</title>
+    <script>
+        var require = {
+            baseUrl: '/',
+            paths: {
+                'vue': '//cdn.jsdelivr.net/npm/vue/dist/vue'
+            }
+        };
+		
+		loadCss = function(config) {
+			var head = document.getElementsByTagName('head')[0];
+		 
+			if (config.content) {
+				var style  = document.createElement('style');
+				style.type = 'text/css';
+				
+				if (style.styleSheet)
+					style.styleSheet.cssText = config.content;
+				else
+					style.innerHTML = config.content;
+		 
+				head.appendChild(style);
+			} else if (config.url) {
+				var link  = document.createElement('link');
+		 
+				link.href = config.url;
+				link.rel  = 'stylesheet';
+				link.type = 'text/css';
+				head.appendChild(link);
+			}
+		};
+    </script>
+    <script data-main="js/app" src="//cdnjs.cloudflare.com/ajax/libs/require.js/2.3.5/require.js"></script>
+</head>
+<body>
+	<div id="app">
+		{{ message }}
+		<hello>Bob</hello>
+		<hello2 name="Jim"></hello2>
+	</div>
+</body>
+</html>
+```
---
+++ index.js
@@ -0,0 +1,125 @@
+var fs          = require('fs');
+var gutil       = require('gulp-util');
+var less        = require('less');
+var parse5      = require('parse5');
+var sass        = require('node-sass');
+var through     = require('through2');
+
+var PLUGIN_NAME = 'gulp-vue-single-file-component';
+var LOG_PREFIX  = '[' + PLUGIN_NAME + ']';
+
+function getAttribute(node, name) {
+    var attr = node.attrs.filter(function(attr) {
+        return attr.name == name;
+    });
+
+    return attr.length ? attr[0].value : null;
+}
+    
+function minify(input) {
+    return input.split('\n').map(function(line) {
+        return line.trim();
+    }).filter(function(line) {
+        return line != '';
+    }).join('');
+}
+
+module.exports = function(options) {
+    var defaults = {
+        debug: false, // Debug mode
+        loadCssMethod: 'require.loadCss' // Load css method name
+    };
+
+    var settings = Object.assign({}, defaults, options);
+
+    return through.obj(function(file, encoding, callback) {
+        if (file.isNull()) {
+            return callback(null, file);
+        }
+
+        if (file.isStream()) {
+            this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Cannot use streamed files'));
+            return callback();
+        }
+
+        if (settings.debug) {
+            console.log(LOG_PREFIX, 'File =>', file.path);
+        }
+        
+        // Parse the the file content and get the tag content
+        var fragment = parse5.parseFragment(file.contents.toString(encoding), {
+            locationInfo: true
+        }), tagContent = [];
+        
+        fragment.childNodes.forEach(function(node) {
+            if (node.tagName == 'style') {
+                var style = parse5.serialize(node),
+                    lang = getAttribute(node, 'lang'),
+                    href = getAttribute(node, 'href');
+
+                if (lang == 'less') {
+                    less.render(style, { compress: true }, function(e, result) {
+                        tagContent['style'] = '{ content: \'' + result.css + '\' }';
+                    });
+                } else if (lang == 'sass' || lang == 'scss') {
+                    var options = {
+                        outputStyle: "compressed",
+                        indentedSyntax: lang == "sass",
+                    };
+
+                    if (href) {
+                        options.file = href;
+                    } else {
+                        options.data = minify(style);
+                    }
+
+                    var result = sass.renderSync(options);
+
+                    tagContent['style'] = '{ content: \'' + result.css.toString().replace('\n', '') + '\' }';
+                } else {
+                    if (href) {
+                        tagContent['style'] = '{ url: "' + href + '" }';
+                    } else {
+                        tagContent['style'] = '{ content: \'' + minify(style) + '\' }';
+                    }
+                }
+            } else if (node.tagName == 'template') {
+                var template,
+                    include = getAttribute(node, 'include');
+                
+                if (include) {
+                    template = fs.readFileSync(include, 'utf-8');
+                } else {
+                    var treeAdapter = parse5.treeAdapters.default,
+                        docFragment = treeAdapter.createDocumentFragment();
+
+                    treeAdapter.appendChild(docFragment, node);
+
+                    template = parse5.serialize(docFragment);
+                    template = template.replace('<template>', '').replace('</template>', '');
+                }
+
+                tagContent['template'] = minify(template.replace(/'/g, '&#39;'));
+            } else if (node.tagName === 'script') {
+                tagContent['script'] = parse5.serialize(node);
+            }
+        });
+        
+        // Build up the file content
+        var content = tagContent['script'];
+
+        // Add a beforeCreate event to load the CSS (if one is specified and not one already)
+        if (tagContent['style'] && !content.includes('beforeCreate()')) {
+            content = content.replace(/(export default [^{]*{)/, '$1\n		beforeCreate() {\n			' + settings.loadCssMethod + '('+ tagContent['style'] + ');\n		},');
+        }
+
+        // Add the template (if one is specified and not one already)
+        if (tagContent['template'] && !content.includes('template:')) {
+            content = content.replace(/(export default [^{]*{)/, '$1\n		template: \'' + tagContent['template'] + '\',');
+        }
+
+        file.contents = new Buffer(content);
+        
+        callback(null, file);
+    });
+}
